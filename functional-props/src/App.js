d4fb14132be7eab068f30d1772039288ca426cf1
triciasykes <tlsykes98@gmail.com>
2022-12-15 11:50:06 -0800

    adds functional props lecture notes

---
+++ functional-props/src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+import React, { useState } from 'react'
+import FoodItem from "./components/FoodItem";
+
+const App = () => {
+
+  const [menu, setMenu] = useState([
+    { name: "Lofthaus Sugar Cookies", ordered: false },
+    { name: "Prime Rib", ordered: false},
+    { name: "Eggnog", ordered: true},
+    { name: "Mac & Cheese", ordered: false},
+    { name: "Lumpia", ordered:true},
+
+  ])
+//  set selectedItem to represent the index of the clicked on food item
+  const orderMenuItem =(selectedItem)=> {
+    menu[selectedItem].ordered = true
+    setMenu([...menu])
+  }
+
+
+  return (
+    <>
+      <h1>Hotel Holiday Happening</h1>
+      <h3>Menu</h3>
+      {menu.map((value, index)=> {
+        return(
+          <FoodItem 
+            foodItem={value} 
+            orderMenuItem={orderMenuItem}
+            index={index}
+         
+          />
+        )
+      }
+      ) }
+    </>
+  );
+}
+
+export default App;
